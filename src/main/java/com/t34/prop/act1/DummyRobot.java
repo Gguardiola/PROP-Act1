5cad42416d73985887c1c3a83c45f16ecfc89eda
Gx3 Studios <gabriel.guardiola@estudiantat.upc.edu>
2022-09-28 09:05:45 +0200

    Ver-0.1

--- src/main/java/com/t34/prop/act1/DummyRobot.java
+++ src/main/java/com/t34/prop/act1/DummyRobot.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.t34.prop.act1;
 import robocode.HitByBulletEvent;
 import robocode.Robot;
@@ -10,7 +5,7 @@ import robocode.ScannedRobotEvent;
 
 /**
  *
- * @author USUARIO
+ * @author T34
  */
 public class DummyRobot extends Robot {
 
